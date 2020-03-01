c5b335f8dfe74f2f42ba63356b5814001cc0f3a9
anubh <anubhavverma11111@gmail.com>
2020-03-01 13:36:47 +0530

    log inside Service

--- src/main/java/com/example/spring_demo/com/example/spring_demo/dto/Customer.java
+++ src/main/java/com/example/spring_demo/com/example/spring_demo/dto/Customer.java
@@ -11,4 +11,5 @@ public class Customer implements Serializable {
     private String id;
     private String name;
     private String salary;
+
 }
--- src/main/java/com/example/spring_demo/serviceDemo/ServiceDemo1.java
+++ src/main/java/com/example/spring_demo/serviceDemo/ServiceDemo1.java
@@ -1,16 +1,18 @@
 package com.example.spring_demo.serviceDemo;
 
 import com.example.spring_demo.com.example.spring_demo.dto.Customer;
-import org.springframework.context.annotation.Configuration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 @Service
+@Slf4j
 public class ServiceDemo1 {
 
     private ArrayList al;
     public ArrayList getIndex(){
         al=new ArrayList();
+       log.info("Inside Service");
         al.add(new Customer("1","Anu","1222222"));
         return al;
     }
